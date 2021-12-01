79757cecbea1668a6021ea99ed9f4c5bc647cf6b
Nicole Huang <nhuang@lcs.on.ca>
2021-12-01 09:56:39 -0500

    Initial Commit

---
+++ FiguresHelper/FiguresHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresHelperApp.swift
+//  FiguresHelper
+//
+//  Created by Nicole on 2021/12/1.
+//
+
+import SwiftUI
+
+@main
+struct FiguresHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

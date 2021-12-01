cc384cc2b60d3f9db19d0d9638b913893bcd535c
Nicole Huang <nhuang@lcs.on.ca>
2021-12-01 10:06:32 -0500

    Start a interface with 3 lines of text and a slide bar

--- FiguresHelper/ContentView.swift
+++ FiguresHelper/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading,spacing: 15){
+            
+            Text("Circle")
+                .font(.title)
+            
+            Text("Radius")
+            
+            HStack{
+                Spacer()
+            Text("35.0")
+                Spacer()
+                
+            }
+            
+            Slider(value: .constant(50.0),
+                   in: 0.0...100.0,
+                   label: {
+                        Text("Opacity")
+            },
+                   minimumValueLabel: {
+                        Text("0")
+            },
+                   maximumValueLabel: {
+                        Text("100")
+            })
+            
+       Spacer()
+        }
     }
 }
 
